c60e2e1e61b582170b1352c29f2ab07dfc67dffe
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2021-07-21 17:05:42 -0300

    feat: settings jest and lint

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-undef */
 // jest.config.js
 
-module.exports = {  
-    transformIgnorePatterns: ['node_modules/(?!(sucrase)/)'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
-    },
-    // ...the rest of your config
-  }
+module.exports = {
+  moduleDirectories: ['node_modules', 'src'],
+  transformIgnorePatterns: ['node_modules/(?!(sucrase)/)'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
+  },
+  bail: 1,
+  verbose: true,
+};
--- src/components/mmc/mmc.spec.ts
+++ src/components/mmc/mmc.spec.ts
@@ -1,8 +1,8 @@
 // Testing MmcFraction
-import MmcFraction from './index'
+import MmcFraction from './index';
 
 describe('test mmcFraction', () => {
-    it('Test mmcFraction', () => {
-        expect(MmcFraction(12,9)).toBe(36);
-    });
-})
+  it('Test mmcFraction', () => {
+    expect(MmcFraction(12, 9)).toBe(36);
+  });
+});
