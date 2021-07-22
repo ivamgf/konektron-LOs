dcf85ba64c0ecf65d6dc6b2bb12cdbc6930cba7b
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2021-07-22 12:32:15 -0300

    feat: settings

--- src/pages/api/metadataApi/index.ts
+++ src/pages/api/metadataApi/index.ts
@@ -6,9 +6,8 @@ import { metadataLom } from '../../../utils/metadata-lom';
 const handler = (_req?: NextApiRequest, res?: NextApiResponse) => {
   try {
     if (!Array.isArray(metadataLom)) {
-      throw new Error('Cannot find user data');
+      throw new Error('Cannot find data');
     }
-
     res.status(200).json(metadataLom);
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message });
